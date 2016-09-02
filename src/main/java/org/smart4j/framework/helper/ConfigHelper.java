5b57fd1ec61ef0b43c74fbd35dceada757b803c6
mcfell <448194192@qq.com>
2016-09-03 00:43:09 +0800

    add classload Class

---
+++ src/main/java/org/smart4j/framework/helper/ConfigHelper.java
@@ -0,0 +1,41 @@
+package org.smart4j.framework.helper;
+
+import org.smart4j.framework.ConfigConstant;
+import org.smart4j.framework.util.PropUtils;
+
+import java.util.Properties;
+
+/**
+ * Created by Administrator on 2016/9/2.
+ */
+public class ConfigHelper {
+    private static final Properties CONFIG_PROPS = PropUtils.loadProps(ConfigConstant.CONFIG_FILE);
+
+    public static String getJdbcDriver() {
+        return PropUtils.getString(CONFIG_PROPS,ConfigConstant.JDBC_DRIVER);
+    }
+
+    public static String getJdbcUrl() {
+        return PropUtils.getString(CONFIG_PROPS,ConfigConstant.JDBC_URL);
+    }
+
+    public static String getJdbcPassWord() {
+        return PropUtils.getString(CONFIG_PROPS,ConfigConstant.JDBC_PASSWORD);
+    }
+
+    public static String getJdbcUserName() {
+        return PropUtils.getString(CONFIG_PROPS,ConfigConstant.JDBC_USERNAME);
+    }
+
+    public static String getAppBasePackage() {
+        return PropUtils.getString(CONFIG_PROPS,ConfigConstant.APP_BASE_PACKAGE);
+    }
+
+    public static String getAppJspPath() {
+        return PropUtils.getString(CONFIG_PROPS,ConfigConstant.APP_JSP_PATH,"/WEB-INF/jsp");
+    }
+
+    public static String getAppAssetPath() {
+        return PropUtils.getString(CONFIG_PROPS,ConfigConstant.APP_ASSET_PATH,"/asset/");
+    }
+}
---
+++ src/main/java/org/smart4j/framework/util/ClassUtil.java
@@ -0,0 +1,123 @@
+package org.smart4j.framework.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * Created by Mcfell on 2016/9/2.
+ */
+public class ClassUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClassUtil.class);
+
+    /**
+     * 获取类加载器
+     * @return
+     */
+    public static ClassLoader getClassLoader() {
+        return Thread.currentThread().getContextClassLoader();
+    }
+
+    /**
+     * 加载类
+     * @param className
+     * @param isInitialized
+     * @return
+     */
+    public static Class<?> loadClass(String className, boolean isInitialized) {
+        Class<?> cls;
+        try {
+            cls = Class.forName(className,isInitialized,getClassLoader());
+        } catch (ClassNotFoundException e) {
+            LOGGER.error("load class failure",e);
+            throw new RuntimeException(e);
+        }
+        return cls;
+    }
+
+    public static Set<Class<?>> getClassSet(String packageName) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        try {
+            Enumeration<URL> urls = getClassLoader().getResources(packageName.replace(".","/"));
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                if (url != null) {
+                    String protocol = url.getProtocol();
+                    if (protocol.equals("file")) {
+                        String packagePath = url.getPath().replaceAll("%20"," ");
+                        addClass(classSet, packagePath, packageName);
+                    } else if (protocol.equals("jar")) {
+                        JarURLConnection urlConnection = (JarURLConnection) url.openConnection();
+                        if (urlConnection != null) {
+                            JarFile jarFile = urlConnection.getJarFile();
+                            if (jarFile != null) {
+                                Enumeration<JarEntry> jarEntries = jarFile.entries();
+                                while (jarEntries.hasMoreElements()) {
+                                    JarEntry jarEntry = jarEntries.nextElement();
+                                    String name = jarEntry.getName();
+                                    if (name.endsWith(".class")) {
+                                        String className = name.substring(0, name.lastIndexOf(".")).replaceAll("/", ".");
+                                        doAddClass(classSet, className);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        } catch (IOException e) {
+            LOGGER.error("get class set failure",e);
+            throw new RuntimeException(e);
+        }
+        return classSet;
+    }
+
+
+    private static void addClass(Set<Class<?>> classSet, String packagePath, String packageName) {
+        File[] files = new File(packagePath).listFiles(new FileFilter() {
+            public boolean accept(File file) {
+                return (file.isFile() && file.getName().endsWith(".class")) || file.isDirectory();
+            }
+        });
+
+        for (File file : files) {
+            String fileName = file.getName();
+            if (file.isFile()) {
+                String classname = fileName.substring(0,fileName.lastIndexOf("."));
+                if (StringUtil.isNotEmpty(packageName)) {
+                    classname = packageName + "." + classname;
+                }
+                doAddClass(classSet, classname);
+            } else {
+                String subPackagePath = fileName;
+                if (StringUtil.isNotEmpty(packagePath)) {
+                    subPackagePath = packagePath + "/" +subPackagePath;
+                }
+                String subPackageName = fileName;
+                if (StringUtil.isNotEmpty(packageName)) {
+                    subPackageName = packageName + "." + subPackageName;
+                }
+                addClass(classSet, subPackagePath, subPackageName);
+            }
+        }
+    }
+
+    private static void doAddClass(Set<Class<?>> classSet, String classname) {
+        Class<?> cls = loadClass(classname, false);
+        classSet.add(cls);
+    }
+
+}
